cc5b1923b85e13114081ed885b3e4ebe6b5e750c
Daniel Furelos Blanco <danielfb93@gmail.com>
2018-04-18 16:55:18 +0200

    refactor precondition checking

--- validator/Plan.h
+++ validator/Plan.h
@@ -14,35 +14,16 @@ public:
     PlanAction( const std::string& name, const StringVec& params )
         : name( name ), params( params ) {}
 
-    bool holds( State& s, Domain * d ) {
-        Action * action = d->actions.get( name );
-        Condition * actionPre = action->pre;
-
-        // hacer analisis recursivo de las condiciones!
-
-        And * a = dynamic_cast< And * >( actionPre );
-
-        for ( unsigned i = 0; i < a->conds.size(); ++i ) {
-            Ground * g = dynamic_cast< Ground * >( a->conds[i] );
-            if ( g ) {
-                const std::string& groundName = g->name;
-                const IntVec& groundParams = g->params;
-
-                StringVec groundParamsObj;
-                for ( unsigned j = 0; j < groundParams.size(); ++j ) {
-                    groundParamsObj.push_back( params[groundParams[j]] );
-                }
-
-                if ( !s.holds( false, groundName, groundParamsObj ) ) {
-                    return false;
-                }
-            }
+    bool holds( State * s, Domain * d ) {
+        Action * a = d->actions.get( name );
+        if ( a ) {
+            return holdsRec( s, d, a->pre );
         }
 
-        return true;
+        return false; // add warning -> the action does not exist!
     }
 
-    void apply( State& s, Domain * d ) {
+    void apply( State * s, Domain * d ) {
         Action * action = d->actions.get( name );
         Condition * actionEff = action->eff;
 
@@ -59,7 +40,7 @@ public:
                     groundParamsObj.push_back( params[groundParams[j]] );
                 }
 
-                s.addFluent( groundName, groundParamsObj );
+                s->addFluent( groundName, groundParamsObj );
             }
 
             Not * n = dynamic_cast< Not * >( a->conds[i] );
@@ -73,7 +54,7 @@ public:
                     groundParamsObj.push_back( params[groundParams[j]] );
                 }
 
-                s.removeFluent( groundName, groundParamsObj );
+                s->removeFluent( groundName, groundParamsObj );
             }
         }
     }
@@ -89,6 +70,43 @@ public:
         }
         stream << ")\n";
         return stream;
+    }
+
+protected:
+    bool holdsRec( State * s, Domain * d, Condition * c ) {
+        And * a = dynamic_cast< And * >( c );
+        if ( a ) {
+            for ( unsigned i = 0; i < a->conds.size(); ++i ) {
+                if ( !holdsRec( s, d, a->conds[i] ) ) {
+                    return false;
+                }
+            }
+        }
+
+        Ground * g = dynamic_cast< Ground * >( c );
+        if ( g ) {
+            if ( !s->holds( false, g->name, getObjectParameters( g->params ) ) ) {
+                return false;
+            }
+        }
+
+        Not * n = dynamic_cast< Not * >( c );
+        if ( n ) {
+            Ground * ng = n->cond;
+            if ( !s->holds( false, ng->name, getObjectParameters( ng->params ) ) ) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    StringVec getObjectParameters( IntVec& groundParams ) {
+        StringVec objParams;
+        for ( unsigned j = 0; j < groundParams.size(); ++j ) {
+            objParams.push_back( params[groundParams[j]] );
+        }
+        return objParams;
     }
 };
 
@@ -133,32 +151,33 @@ public:
 
     void validate( Domain * d, Instance * ins ) {
         // build initial state
-        State initialState( d, ins );
+        State * currentState = new State( d, ins );
 
         // validar each action depending on the current state and create new state if preconditions hold
         // throw exception if validation fails
         bool validSeq = true;
 
-        for ( unsigned i = 0; i < actions.size(); ++i ) {
-            if ( actions[i].holds( initialState, d ) ) {
-                actions[i].apply( initialState, d );
+        for ( unsigned i = 0; i < actions.size() && validSeq; ++i ) {
+            if ( actions[i].holds( currentState, d ) ) {
+                actions[i].apply( currentState, d );
             }
             else {
                 std::cout << "invalid plan\n";
                 validSeq = false;
-                break;
             }
         }
 
         //comprobar si se cumplen las condiciones de la meta!
         if ( validSeq ) {
-            if ( initialState.satisfiesGoal( d, ins ) ) {
+            if ( currentState->satisfiesGoal( d, ins ) ) {
                 std::cout << "SAT!\n";
             }
             else {
                 std::cout << "UNSAT\n";
             }
         }
+
+        delete currentState;
     }
 
     friend std::ostream& operator<<( std::ostream& os, const Plan& p ) {
