b92261d72c815dc9de1c775420504908bb015e55
Daniel Furelos Blanco <danielfb93@gmail.com>
2018-04-19 17:22:18 +0200

    support for when condition

--- validator/PlanAction.h
+++ validator/PlanAction.h
@@ -4,6 +4,8 @@
 
 using namespace parser::pddl;
 
+typedef std::vector< std::pair< std::string, StringVec > > GroundedObjVec;
+
 class PlanAction {
 public:
     std::string name;
@@ -24,7 +26,16 @@ public:
     void apply( State * s, Domain * d ) {
         Action * a = d->actions.get( name );
         if ( a ) {
-            applyRec( s, d, a->eff );
+            GroundedObjVec addList, deleteList;
+            applyRec( s, d, a->eff, addList, deleteList );
+
+            for ( auto it = addList.begin(); it != addList.end(); ++it ) {
+                s->addFluent( it->first, it->second );
+            }
+
+            for ( auto it = deleteList.begin(); it != deleteList.end(); ++it ) {
+                s->removeFluent( it->first, it->second );
+            }
         }
     }
 
@@ -105,47 +116,55 @@ protected:
         return true;
     }
 
-    void applyRec( State * s, Domain * d, Condition * c ) {
+    void applyRec( State * s, Domain * d, Condition * c, GroundedObjVec& addList, GroundedObjVec& deleteList ) {
         And * a = dynamic_cast< And * >( c );
         if ( a ) {
             for ( unsigned i = 0; i < a->conds.size(); ++i ) {
-                applyRec( s, d, a->conds[i] );
+                applyRec( s, d, a->conds[i], addList, deleteList );
             }
         }
 
         Ground * g = dynamic_cast< Ground * >( c );
         if ( g ) {
-            s->addFluent( g->name, getObjectParameters( g->params ) );
+            addList.push_back( std::make_pair( g->name, getObjectParameters( g->params ) ) );
         }
 
         Not * n = dynamic_cast< Not * >( c );
         if ( n ) {
             Ground * ng = n->cond;
-            s->removeFluent( ng->name, getObjectParameters( ng->params ) );
+            deleteList.push_back( std::make_pair( ng->name, getObjectParameters( ng->params ) ) );
         }
 
         Forall * f = dynamic_cast< Forall * >( c );
         if ( f ) {
             StringVec forallParams = d->typeList( f );
-            forallApplyRec( forallParams, 0, s, d, f->cond );
+            forallApplyRec( forallParams, 0, s, d, f->cond, addList, deleteList );
+        }
+
+        When * w = dynamic_cast< When * >( c );
+        if ( w ) {
+            bool conditionHolds = holdsRec( s, d, w->pars );
+            if ( conditionHolds ) {
+                applyRec( s, d, w->cond, addList, deleteList );
+            }
         }
     }
 
-    void forallApplyRec( const StringVec& forallParams, unsigned paramIndex, State * s, Domain * d, Condition * c ) {
+    void forallApplyRec( const StringVec& forallParams, unsigned paramIndex, State * s, Domain * d, Condition * c, GroundedObjVec& addList, GroundedObjVec& deleteList ) {
         if ( paramIndex < forallParams.size() ) {
             Type * type = d->getType( forallParams[paramIndex] );
 
             for ( unsigned i = 0; i < type->noObjects(); ++i ) {
                 std::pair< std::string, int > typeObj = type->object( i );
                 params.push_back( typeObj.first );  // expand action params
-                forallApplyRec( forallParams, paramIndex + 1, s, d, c );
+                forallApplyRec( forallParams, paramIndex + 1, s, d, c, addList, deleteList );
                 params.pop_back();  // remove previously added params
             }
         }
         else {
             // all forall parameters instantiated and added to action params,
             // continue checking satisfiability
-            applyRec( s, d, c );
+            applyRec( s, d, c, addList, deleteList );
         }
     }
 
